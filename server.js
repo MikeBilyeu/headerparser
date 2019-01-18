d7f9fd84c3885395ca3d9f847ff823110046284f
Michael Bilyeu <Bilyeu@Michaels-iMac.local>
2019-01-18 12:01:06 -0500

    Initial commit

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var express = require('express');
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
+// so that your API is remotely testable by FCC
 var cors = require('cors');
 app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
 
@@ -19,14 +19,22 @@ app.get("/", function (req, res) {
 });
 
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+// your first API endpoint...
+app.get("/api/whoami", function (req, res) {
+  let ipadress = req.header('x-forwarded-for') || req.connection.remoteAddress;
+  let software = req.headers['user-agent'];
+  let language = req.headers['accept-language'];
+  let userWhoAmI = {
+    ipadress,
+    language,
+    software
+  };
+  res.json(userWhoAmI);
 });
 
 
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+var listener = app.listen(3000, function () {
+  console.log('Your app is listening on port 3000');
 });
